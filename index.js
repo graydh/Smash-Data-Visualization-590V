020d20a37a978801d1451570c68043f5838dfb57
Eli Gukovsky <gukovskye@gmail.com>
2020-03-30 18:22:34 -0500

    Create matchup table from database

--- index.js
+++ index.js
@@ -1,10 +1,93 @@
 const path = require("path");
 const express = require("express");
-const { default: SlippiGame } = require("slp-parser-js");
+const { characters, default: SlippiGame} = require("slp-parser-js");
+const MongoClient = require("mongodb").MongoClient;
 
 const app = express();
 
 let port = process.env.PORT || 3000;
+
+app.get("/allchars", function(req, res) {
+	console.log("Requested: /allchars");
+	res.json(characters.getAllCharacters());
+});
+
+app.get("/matchup", function(req, res) {
+	MongoClient.connect("mongodb://127.0.0.1:27017/", {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	})
+	.then(client => {
+		let db = client.db("smashdb");
+		return db.collection("sets").aggregate(
+			[
+				{
+					$unwind: "$games"
+				},
+				{
+					$match: {
+						$or: [
+							{
+								"games.winner": "player1",
+								"games.player1.character": req.query.winner,
+								"games.player2.character": req.query.loser,
+							},
+							{
+								"games.winner": "player2",
+								"games.player1.character": req.query.loser,
+								"games.player2.character": req.query.winner,
+							}
+						]
+					}
+				},
+				{
+					$project: {
+						"winner": {
+							$cond: {
+								if: {
+									$eq: [ "$games.winner", "player1" ]
+								},
+								then: {
+									"tag": "$player1",
+									"character": "$games.player1.character"
+								},
+								else: {
+									"tag": "$player2",
+									"character": "$games.player2.character"
+								}
+							}
+						},
+						"loser": {
+							$cond: {
+								if: {
+									$eq: [ "$games.winner", "player1" ]
+								},
+								then: {
+									"tag": "$player2",
+									"character": "$games.player2.character"
+								},
+								else: {
+									"tag": "$player1",
+									"character": "$games.player1.character"
+								}
+							}
+						},
+						"stage": "$games.stage"
+					}
+				}
+			]
+		);
+	})
+	.then(result => {
+		return result.toArray();
+	})
+	.then(result => {
+		res.send(result);
+	})
+	.catch(err => {
+		console.error(err);
+	});
+})
 
 // Request a specific game
 // app.get("/replay", function(req, res) {
