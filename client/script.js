88160497529d02b1b07420aec51464731dd473a6
Eli Gukovsky <gukovskye@gmail.com>
2020-03-30 18:22:15 -0500

    Begin work on client side

--- client/script.js
+++ client/script.js
@@ -1,1 +1,44 @@
-// Use d3 here
+d3.select("#nav-matchups").on("click", function() {
+	let handler = function() {
+		// TODO
+		console.log("handler");
+		console.log(d3.select(this));
+	};
+	d3.json("/allchars")
+	.then(function (json) {
+		let options = d3.select("#options");
+		options.append("label")
+			.text("Winner: ")
+			.attr("for", "options-winner");
+		let winnerDropdown = options.append("select")
+			.attr("id", "options-winner")
+			.on("change", handler);
+		winnerDropdown.selectAll("option")
+			.data(json)
+			.enter()
+			.append("option")
+			.attr("value", function (d) {
+				return d.shortName;
+			})
+			.text(function (d) {
+				return d.name;
+			});
+		options.append("br");
+		options.append("label")
+			.text("Loser: ")
+			.attr("for", "options-loser");
+		let loserDropdown = options.append("select")
+			.attr("id", "options-loser")
+			.on("change", handler);
+		loserDropdown.selectAll("option")
+			.data(json)
+			.enter()
+			.append("option")
+			.attr("value", function (d) {
+				return d.shortName;
+			})
+			.text(function (d) {
+				return d.name;
+			});
+	});
+});
