120d99d8679adde1de2864d16dfbd293c41f886e
ayushsahu <ayushsahu@umass.edu>
2020-04-20 14:46:05 -0400

    finished game data and added twitter bootstrap

--- client/scripts/games.js
+++ client/scripts/games.js
@@ -1,14 +1,8 @@
+
 d3.select("#nav-games").on("click", function () {
-	d3.select("#options").html("");
-	d3.select("#visualization").html("");
-	let textw = 200;
-	let graphw = 400;
-	let w = textw + graphw;
-	let h = 400;
-	let padding = 10;
 
+	const tableDiv = d3.select('#visualization').append('div').attr('id', 'tableContainer').attr('class', 'well');
 	let options = d3.select("#options");
-
 	options.append("label")
 		.text("Player 1: ")
 		.attr("id", "label-options-player1");
@@ -28,18 +22,32 @@ d3.select("#nav-games").on("click", function () {
 		.text("Select Game: ")
 		.attr("id", "label-select-game");
 	options.append("br");
-
-
+	let games;
+	let renderer3 = function () {
+		let gamenum = d3.select("#select-game").property("value");
+		let game = games[gamenum - 1];
+		let data = [];
+		console.log(game);
+		for (key of Object.keys(game.player1)) {
+			let obj = {
+				player1: game.player1[key],
+				player2: game.player2[key],
+				metric: key
+			};
+			data.push(obj);
+		}
+		console.log(data);
+		tablulate(data);
+	}
 	let renderer2 = function () {
 		let set = d3.select("#select-set").property("value");
-		
+
 		d3.select("#select-game").selectAll("option").remove();
-		
-		if(set ==="") return;
+		if (set === "") return;
 
 		d3.json(`/gamedata?id=${set}`)
 			.then(function (json) {
-				let games = json[0].games;
+				games = json[0].games;
 
 				console.log(games);
 				gameDropdown.selectAll("option")
@@ -52,14 +60,14 @@ d3.select("#nav-games").on("click", function () {
 					.text(function (d, i) {
 						return "game " + i;
 					});
-			});
+			}).then(renderer3);
 	}
 
 	let renderer1 = function () {
+		d3.selectAll('table').remove();
 		let player1 = d3.select("#options-player1").property("value");
 		let player2 = d3.select("#options-player2").property("value");
 		d3.select("#select-set").selectAll("option").remove();
-
 		d3.json(`/gamedata?player1=${player1}&player2=${player2}`)
 			.then(function (json) {
 				let sets = json;
@@ -90,7 +98,7 @@ d3.select("#nav-games").on("click", function () {
 
 	let gameDropdown = d3.select("#label-select-game").append("select")
 		.attr("id", "select-game")
-		.on("change", renderer2);
+		.on("change", renderer3);
 
 	d3.json("/allplayers")
 		.then(function (json) {
@@ -124,67 +132,38 @@ d3.select("#nav-games").on("click", function () {
 			options.append("br");
 		})
 		.then(renderer1);
+
+	function tablulate(data) {
+		d3.selectAll('table').remove();
+		columns = ["metric", "player1", "player2"];
+		var table = tableDiv.append('table');
+		table.attr('class', 'table table-condensed table-striped table-bordered');
+		var thead = table.append('thead')
+		var tbody = table.append('tbody')
+
+		thead.append('tr')
+			.selectAll('th')
+			.data(columns)
+			.enter()
+			.append('th')
+			.text(function (d) { return d })
+
+		var rows = tbody.selectAll('tr')
+			.data(data)
+			.enter()
+			.append('tr')
+
+		var cells = rows.selectAll('td')
+			.data(function (row) {
+				return columns.map(function (column) {
+					return { column: column, value: row[column] }
+				})
+			})
+			.enter()
+			.append('td')
+			.text(function (d) { return d.value })
+
+		return table;
+	}
+
 });
-
-
-
-
-
-// var movies = [
-// 	{ title: "The Godfather", year: 1972, length: 175, budget: 6000000, rating: 9.1 },
-// 	{ title: "The Shawshank Redemption", year: 1994, length: 142, budget: 25000000, rating: 9.1 },
-// 	{ title: "The Lord of the Rings: The Return of the King", year: 2003, length: 251, budget: 94000000, rating: 9 },
-// 	{ title: "The Godfather: Part II", year: 1974, length: 200, budget: 13000000, rating: 8.9 },
-// 	{ title: "Shichinin no samurai", year: 1954, length: 206, budget: 500000, rating: 8.9 },
-// 	{ title: "Buono, il brutto, il cattivo, Il", year: 1966, length: 180, budget: 1200000, rating: 8.8 },
-// 	{ title: "Casablanca", year: 1942, length: 102, budget: 950000, rating: 8.8 },
-// 	{ title: "The Lord of the Rings: The Fellowship of the Ring", year: 2001, length: 208, budget: 93000000, rating: 8.8 },
-// 	{ title: "The Lord of the Rings: The Two Towers", year: 2002, length: 223, budget: 94000000, rating: 8.8 },
-// 	{ title: "Pulp Fiction", year: 1994, length: 168, budget: 8000000, rating: 8.8 }
-// ];
-
-// // column definitions
-// var columns = [
-// 	{ head: 'Movie title', cl: 'title', html: ƒ('title') },
-// 	{ head: 'Year', cl: 'center', html: ƒ('year') },
-// 	{ head: 'Length', cl: 'center', html: ƒ('length', length()) },
-// 	{ head: 'Budget', cl: 'num', html: ƒ('budget', d3.format('$,')) },
-// 	{ head: 'Rating', cl: 'num', html: ƒ('rating', d3.format('.1f')) }
-// ];
-
-// // create table
-// var table = d3.select('body')
-// 	.append('table');
-
-// // create table header
-// table.append('thead').append('tr')
-// 	.selectAll('th')
-// 	.data(columns).enter()
-// 	.append('th')
-// 	.attr('class', ƒ('cl'))
-// 	.text(ƒ('head'));
-
-// // create table body
-// table.append('tbody')
-// 	.selectAll('tr')
-// 	.data(movies).enter()
-// 	.append('tr')
-// 	.selectAll('td')
-// 	.data(function (row, i) {
-// 		return columns.map(function (c) {
-// 			// compute cell values for this specific row
-// 			var cell = {};
-// 			d3.keys(c).forEach(function (k) {
-// 				cell[k] = typeof c[k] == 'function' ? c[k](row, i) : c[k];
-// 			});
-// 			return cell;
-// 		});
-// 	}).enter()
-// 	.append('td')
-// 	.html(ƒ('html'))
-// 	.attr('class', ƒ('cl'));
-
-// function length() {
-// 	var fmt = d3.format('02d');
-// 	return function (l) { return Math.floor(l / 60) + ':' + fmt(l % 60) + ''; };
-// }
