4e91ad1039b73881d6fa69c3e7930fb7485c9080
Declan Gray-Mullen <dgraymullen@umass.edu>
2020-04-21 21:37:43 -0400

    Finished Players Stats Visualization

--- client/scripts/players.js
+++ client/scripts/players.js
@@ -1,11 +1,11 @@
 d3.select("#nav-players").on("click", function () {
 	d3.select("#options").html("");
 	d3.select("#visualization").html("");
-	let textw = 200;
-	let graphw = 400;
+	let textw = 100;
+	let graphw = 500;
 	let w = textw + graphw;
 	let h = 400;
-	let padding = 10;
+	let padding = 5;
 	let players = [];
 	let svg = d3.select("#visualization")
 		.append("svg")
@@ -16,83 +16,91 @@ d3.select("#nav-players").on("click", function () {
 	let g_graph = svg
 		.append("g")
 		.attr("transform", `translate(${textw}, 0)`);
+	g_graph.append("g")
+		.attr("class", "xAxis")
+		.attr("transform", "translate(0,-1)"); //move xAxis Up 1 pixel
 
 
 	let renderer = function () {
 		let player = d3.select("#options-player").property("value");
 		let metric = d3.select("#options-metric").property("value");
+		let order = d3.select("#options-order").property("value");
 
-		console.log(players);
 		let playerArray = [];
+		d3.json(`/averages?metric=${metric}`)
+			.then(function (json) {
+				let playerArray = json;
 
-		for (let playerName of players) {
-			d3.json(`/playerdata?player=${playerName}`)
-				.then(function (json) {
-					playerObject = new Object();
-					playerObject["player"] = playerName;
-					let games = 0;
-					let total_metric = 0;
-					for (let game of json) {
-						if (game[metric] !== null)
-							games += 1;
-							total_metric += game[metric];
-					}
-					playerObject[metric] = total_metric/games;
-					
-					playerArray.push(playerObject);
-				});
-		}
+				//yScale Domain Ordering
+				let orders = {
+	    			Alphabetical: function(a,b){return a.name.localeCompare(b.name) },
+	    			Ascending: function(a, b){return a.avg - b.avg},
+	   				Descending: function(a, b){return b.avg - a.avg}
+	  			};
 
-		console.log(playerArray);
+				//convert object to needed order
+				playerArray.sort(orders[order]);
+				let sorted_players = playerArray.map(a => a.name);
+				
 
+				//needed for xScale domain
+				let dataMax = playerArray.reduce((a,b)=>a.avg>b.avg?a:b).avg;
+				let dataMin = Math.floor(playerArray.reduce((a,b)=>a.avg<b.avg?a:b).avg);
 
-		let xScale = d3.scaleLinear()
-			.domain([0, 12])
-			.range([0, graphw - 2 * padding]);
+				let xScale = d3.scaleLinear()
+					.domain([dataMin, dataMax])
+					.range([0, graphw - 2 * padding]);
 
-		let yScale = d3.scaleBand()
-			.domain(player)
-			.rangeRound([0, h])
-			.paddingInner(padding / h)
-			.paddingOuter(padding / h * 4);
+				let yScale = d3.scaleBand()
+					.domain(sorted_players)
+					.rangeRound([0, h])
+					.paddingInner(padding / h)
+					.paddingOuter(padding / h * 4);
 
-		let barHeight = (h - padding * (players.length + 1)) / players.length;
+				let barHeight = (h - padding * (players.length + 1)) / players.length;
+				
+				g_graph.selectAll(".left")
+					.data(playerArray)
+					.enter()
+					.append("rect")
+					.attr("class", "left");
+				g_graph.selectAll(".left")
+					.data(playerArray)
+					.transition()
+					.attr("x", padding)
+					.attr("y", function (d, i) {
+						return yScale(d.name);
+					})
+					.attr("width", function (d, i) {
+						return xScale(d.avg);
+					})
+					.attr("height", barHeight)
+					.attr("fill", function (d, i) {
+						if(d.name === player){
+							return "hsl(360, 80%, 50%)"
+						}
+						return "hsl(120, 30%, 50%)"
+					});
+				g_text.selectAll("text")
+					.data(playerArray)
+					.enter()
+					.append("text")
+					.style("dominant-baseline", "middle")
+					.style("text-anchor", "end")
+					.style("font-size", "7px");
+				g_text.selectAll("text")
+					.data(playerArray)
+					.transition()
+					.attr("x", textw)
+					.attr("y", function (d, i) {
+						return yScale(d.name) + barHeight / 2;
+					})
+					.text(function (d, i) {
+						return `${d.name}`;
+					});
+				g_graph.select(".xAxis")
+      				.call(d3.axisBottom(xScale));
 
-		
-		g_graph.selectAll(".left")
-			.data(playerArray)
-			.enter()
-			.append("rect")
-			.attr("class", "left");
-		g_graph.selectAll(".left")
-			.data(playerArray)
-			.transition()
-			.attr("x", padding)
-			.attr("y", function (d, i) {
-				return yScale(d.player);
-			})
-			.attr("width", function (d, i) {
-				return xScale(d[metric]);
-			})
-			.attr("height", barHeight)
-			.attr("fill", function (d, i) {
-				return "hsl(120, 30%, 50%)"
-			});
-		g_text.selectAll("text")
-			.data(playerArray)
-			.enter()
-			.append("text")
-			.style("dominant-baseline", "middle")
-			.style("text-anchor", "end");
-		g_text.selectAll("text")
-			.data(playerArray)
-			.transition()
-			.attr("x", textw)
-			.attr("y", function (d, i) {
-				return yScale(d.player) + barHeight / 2;
-			})
-			.text(function (d, i) {
-				return `${d.player}`;
 			});
 
 	};
@@ -120,6 +128,9 @@ d3.select("#nav-players").on("click", function () {
 					return d;
 				});
 			playerdropdown.property("value", "elicik");
+
+
+
 			options.append("br");
 			options.append("label")
 				.text("Metric: ")
@@ -150,7 +161,6 @@ d3.select("#nav-players").on("click", function () {
 					value: "counterHitRatio"
 				}
 			];
-
 			metricdropdown.selectAll("option")
 				.data(metrics)
 				.enter()
@@ -162,6 +172,34 @@ d3.select("#nav-players").on("click", function () {
 					return d.label;
 				});
 			metricdropdown.property("value", "inputsPerSecond");
+
+
+
+			options.append("br");
+			options.append("label")
+				.text("Order: ")
+				.attr("for", "options-order");
+			options.append("br");
+			let orderdropdown = options.append("select")
+				.attr("id", "options-order")
+				.on("change", renderer);
+			let orders = [
+    			{label: "Alphabetical"},
+    			{label: "Ascending"},
+   				{label: "Descending"}
+  			];
+			orderdropdown.selectAll("option")
+				.data(orders)
+				.enter()
+				.append("option")
+				.attr("value", function (d){
+					return d.label;
+				})
+				.text(function (d) {
+					return d.label;
+				});
+			orderdropdown.property("label", "Alphabetical");
+
 		})
 		.then(renderer);
 });
